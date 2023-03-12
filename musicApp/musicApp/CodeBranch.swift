845cd83542e44f11b131abe8d3df6d1fbd4574bd
Artem <ivanov@softinvent.ru>
2023-03-12 20:56:42 +0300

    braaaaaanch

---
+++ musicApp/musicApp/CodeBranch.swift
@@ -0,0 +1,20 @@
+//
+//  CodeBranch.swift
+//  musicApp
+//
+//  Created by Artyom Ivanov on 12.03.2023.
+//
+
+import SwiftUI
+
+struct CodeBranch: View {
+    var body: some View {
+        Text("New balance")
+    }
+}
+
+struct CodeBranch_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeBranch()
+    }
+}
