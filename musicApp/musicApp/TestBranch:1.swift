898a2d528ae3a306379c906fe4546d99a768d013
Artem <ivanov@softinvent.ru>
2023-03-12 19:53:46 +0300

    testBranch/2

---
+++ musicApp/musicApp/TestBranch:1.swift
@@ -0,0 +1,15 @@
+//
+//  TestBranch:1.swift
+//  musicApp
+//
+//  Created by Artyom Ivanov on 12.03.2023.
+//
+
+import UIKit
+
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
